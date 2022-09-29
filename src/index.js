caed7b298fcdc5a28ba8f34d19ae99ab1999f459
John Doe <johndoe@example.com>
2022-09-29 15:07:42 +0200

    Vége :(

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 import './style.css';
 
+
+function kep(){
+    let cicaskep = document.getElementById('urlbemenet').value;
+    document.getElementById('kephelye').src = "../src" + cicaskep;
+}
+
+function szelssegallit(){
+    let szelesseg = document.getElementById('szeles').value;
+    
+}
+
+function vastagsagallit(){
+    let vastagsag = document.getElementById('vastag').value;
+
+}
+
+function szinallit(){
+    let szinesseg = document.getElementById('szin').value;
+
+}
+
+function init(){
+    document.getElementById('kephelye').addEventListener('change', kep);
+    document.getElementById('kephelye').addEventListener('input', szelssegallit);
+    document.getElementById('kephelye').addEventListener('input', szinallit);
+}
+
+document.addEventListener('DOMContentLoaded', init);
